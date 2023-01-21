b50690e0cd116f316034577b69f9980a183d9199
lida.shahmiri <ls26755@open.ac.uk>
2023-01-21 10:47:28 +0000

    Comparision/comparison How to use formatting/formatting How to write quotation

---
+++ 2-how to use formatting/formatting.py
@@ -0,0 +1,8 @@
+username='lida'
+family='shahmiri'
+num1=5
+num2=3
+print('username is '+username+' '+'familyname is '+ family)
+#formatting------------------------------------
+print(f'username is {username} familyname is {family}')
+print(f'sum={num1+num2}')
---
+++ 3-comparision/comparison.py
@@ -0,0 +1,6 @@
+#comparision between two number
+
+num_1=2
+num_2=3
+print(f'num_1=num_2:{num_1==num_2}')
+print(f'num_1=num_2:{num_1!=num_2}')
