571854d3b2c120494bf9c68abb7aaf4d0f3dbe85
lida.shahmiri <ls26755@open.ac.uk>
2023-04-26 14:34:40 +0100

    initial commit

---
+++ Fibonacci_Sequence/Fibonacci Sequence.py
@@ -0,0 +1,17 @@
+def fibonachi(num):
+    # Initilize the first two numbers of the sequence
+    fib_seq = [0, 1]
+
+    # Loop through each number to n-2 (since we already have the first two number)
+    for i in range(2, num):
+        # Add the previous two numbers in the sequence to get the next number
+        next_num = fib_seq[i-1]+fib_seq[i-2]
+
+        # Add the next number to the list
+        fib_seq.append(next_num)
+    # Return the completed Fibonachi sequence
+    return fib_seq
+
+
+# Print the sequence to the console
+print(fibonachi(10))
